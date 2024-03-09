116e84eb754334f0e22fba642a25e1a81a8b40fb
vipultarur <vipultarur2003@gmail.com>
2024-03-09 15:01:06 +0530

    sixteen

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,10 @@
 echo "BUILD START"
-python manage.py runserver
+#!/bin/bash
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput --clear
+
 echo "BUILD END" 
