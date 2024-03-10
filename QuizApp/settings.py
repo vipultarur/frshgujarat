71ecb99d4d19ab859437fd1cf902ae2872f8027b
vipultarur <vipultarur2003@gmail.com>
2024-03-10 14:47:58 +0530

    13commit

--- QuizApp/settings.py
+++ QuizApp/settings.py
@@ -70,12 +70,9 @@ WSGI_APPLICATION = 'QuizApp.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'libpro',
-        'USER':'postgres',
-        'PASSWORD':'vipul23',
-        'HOST':'localhost',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
